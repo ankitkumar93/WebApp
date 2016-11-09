1786dc1d091762bfd7d91e3ca2e8e971d9377a5b
Vipul Kashyap <vipulkashyap@Vipuls-MacBook-Pro.local>
2016-11-09 17:46:09 -0500

    updated server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express');
 var redis = require('redis');
 var fs = require('fs');
 var publicIP = require('external-ip')();
-
+var myIP;
 // Globals
 var PORT = 8080;
 
@@ -21,12 +21,17 @@ var app = express();
 // Setup Redis
 var redisClient = redis.createClient(6379, config.REDIS_IP, {});
 
+publicIP(function(error, ip) {
+		myIP = ip;
+		redisClient.lpush("production_servers", ip);
+});
+
 // Routing
 app.get('/', function(req, res) {
 		res.write("Hello Devs!\n");
 		redisClient.get('featureFlag', function(err, val) {
 				if (err) res.send(err);
-				else if (val == 'on') res.write("My IP Is: " + config.MY_IP);
+				else if (val == 'on') res.write("My IP Is: " + myIP);
 			   res.end();
 		});
 });
@@ -34,7 +39,4 @@ app.get('/', function(req, res) {
 // Start HTTP Server
 var server = app.listen(PORT, function() {
     console.log("Server Up and Running on Port: " + PORT);
-});
-publicIP(function(error, ip) {
-		redisClient.lpush("production_servers", ip);
 });
