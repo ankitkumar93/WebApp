0ca08b9072ba70c2307fc45813403134556147f3
akumar18 <akumar18@ncsu.edu>
2016-11-11 03:40:25 -0500

    Done

--- server/monitor/monitor.sh
+++ server/monitor/monitor.sh
@@ -4,20 +4,19 @@
 # Globals
 CPU_LIMIT=50
 MEM_LIMIT=80
-ADMIN_EMAIL=""
-redis_ip=$(cat config.json | jq '.REDIS_IP' | tr '"')
 # Monitor
 # Reference Taken from : # http://stackoverflow.com/questions/9229333/how-to-get-overall-cpu-usage-e-g-57-on-linux
+# redis_ip=$(cat config.json | jq '.REDIS_IP' | tr -d '"')
 while [ true ]; do
 		# CPU Usage
 		cpu_usage=$(top -bn1 | grep "Cpu(s)" | cut -d ',' -f4 | sed "s/\([0-9.]*\)* id/\1/" | sed -r "s/\s+//")
-		#cpu_usage = $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 		cpu_usage=${cpu_usage/.*}
-		cpu_usage=$((100-cpu_usage))
 		if [ $cpu_usage -gt $CPU_LIMIT ]; then
 			echo $cpu_usage
 
 			node sendMail.js c
+
+			_autoscale
 
 			exit
 		fi
@@ -34,5 +33,17 @@ while [ true ]; do
 			exit
 		fi
 done
+
+_autoscale() {
+	app_ip=$(node provision/digitalocean.js)
+	echo "[appserver]" > inventory
+	echo 'node ansible_ssh_host='$app_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
+
+	sleep 120
+
+	cp '../config.json' './config.json'
+
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory configure_prod.yml --limit "appserver"
+}
 
 # End of Script
