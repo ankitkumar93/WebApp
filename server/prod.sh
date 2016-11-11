12b1a01c1506417a095325848d9df3503db7c052
akumar18 <akumar18@ncsu.edu>
2016-11-11 02:29:18 -0500

    DO

--- server/prod.sh
+++ server/prod.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 ## Script to Deploy the Redis server to Production ##
-redis_ip=$(node AWS_main.js | tr -d '"')
+redis_ip=$(node provision/digitalocean.js)
 echo '{"REDIS_IP":"'$redis_ip'"}' > config.json
 echo "[redisserver]" > inventory
-echo 'node1 ansible_ssh_host='$redis_ip' ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./key/privateKey.key' >> inventory
+echo 'node1 ansible_ssh_host='$redis_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory configure_prod.yml --limit "redisserver"
 # Script to Deploy the Proxy server to Production ##
-proxy_ip=$(node AWS_main.js | tr -d '"')
+proxy_ip=$(node provision/digitalocean.js)
 echo "[proxyserver]" >> inventory
-echo 'node2 ansible_ssh_host='$proxy_ip' ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./key/privateKey.key' >> inventory
+echo 'node2 ansible_ssh_host='$proxy_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory configure_prod.yml --limit "proxyserver"
 # Script to Deploy the App server to Production ##
-app_ip=$(node AWS_main.js | tr -d '"')
+app_ip=$(node provision/digitalocean.js)
 echo "[appserver]" >> inventory
-echo 'node3 ansible_ssh_host='$app_ip' ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./key/privateKey.key' >> inventory
+echo 'node3 ansible_ssh_host='$app_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> inventory
 
 ## Deploy
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory configure_prod.yml --limit "appserver"
