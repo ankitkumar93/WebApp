2590888a80550b6750e605529a59cc5c270cfdeb
akumar18 <akumar18@ncsu.edu>
2016-11-11 03:18:53 -0500

    Done

--- server/canary.sh
+++ server/canary.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 ## Script to Deploy the App server to Production ##
-redis_ip=$(node provision/digitalocean.js)
+app_ip=$(node provision/digitalocean.js)
 echo "[appserver]" > canary_inventory
 echo 'node ansible_ssh_host='$app_ip' ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa' >> canary_inventory
+
+sleep 120
 
 ## Deploy
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i canary_inventory configure_canary.yml --limit "appserver"
 
-redis_ip=$(cat config.json | jq '.REDIS_IP')
+redis_ip=$(cat config.json | jq '.REDIS_IP' | tr -d '"')
 
 #set canary_on = true on redis server
 redis-cli -h $redis_ip set canary_on true
