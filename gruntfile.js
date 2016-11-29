e8da59b7152c17806a5a58abc7e0bd8ac0db4c56
akumar18 <akumar18@ncsu.edu>
2016-11-28 19:07:34 -0500

    Fixed tests

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        mochaTest : {
+            test : {
+                options : {
+                    ui : 'tdd',
+                    reporter : 'spec',
+                    reporterOptions: {'output' : 'mocha-tests-output.xml'},
+                    timeout: 20000
+                },
+                src : [ './test/*.js' ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('test', [ 'mochaTest' ]);
+};
